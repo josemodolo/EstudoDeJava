a101b89898127fe08446e1f7c2648099f9959211
josemodolo <72349643+josemodolo@users.noreply.github.com>
2023-06-28 16:15:17 -0300

    Add files via upload
    Estudo de atribuição de valores em variáveis e suas formatações

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,11 +1,26 @@
 import java.util.Locale;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		    
+		double price1 = 2100.0;	
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.println(product1 + " which prince is " + price1);
+		System.out.println(product2 + " which prince is " + price2);		
+		System.out.println("Record: " + age + " years old," + " code " + code + " and gender: " + gender);
+		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
 		Locale.setDefault(Locale.US);
-		double x = 1.9505150;
-		System.out.println(x);
-	    System.out.printf("%.2f%n", x);
-	    System.out.printf("%.4f%n", x);
+		System.out.printf("US decimal point: %.3f%n", measure);
+		
 	}
 }
