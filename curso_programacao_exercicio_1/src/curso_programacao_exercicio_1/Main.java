cfa01881fc5c4399c037f96dca2a07645366a6aa
josemodolo <72349643+josemodolo@users.noreply.github.com>
2023-06-30 11:45:10 -0300

    Programa de multiplicação de area

---
+++ curso_programacao_exercicio_1/src/curso_programacao_exercicio_1/Main.java
@@ -0,0 +1,22 @@
+package curso_programacao_exercicio_1;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in);
+             
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado = sc.nextDouble ();
+        
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+        
+        System.out.println("AREA = " + area);
+        System.out.println("PRECO = " + preco);
+        
+    }
+}
