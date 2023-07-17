871a8046cba22fc224743643a4c9ffa687a7f623
josemodolo <72349643+josemodolo@users.noreply.github.com>
2023-07-17 16:14:18 -0300

    Add files via upload

---
+++ curso_programacao_exercicio_2/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in);
+        
+        double pi = 3.14159;
+        double R = sc.nextDouble();
+        double A = ( pi * R * R);
+        
+        System.out.printf("A=%.4f%n", A);
+        
+        sc.close();  
+
+    }
+
+}
